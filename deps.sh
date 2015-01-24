2862f65c32d4ff740e0146fe72854e3dabdcbd5c
Mattia <mattia.asti@gmail.com>
2015-01-24 17:02:10 +0000

    removed sudo

--- deps.sh
+++ deps.sh
@@ -3,4 +3,4 @@
 set -o errexit
 
 apt-get update 
-apt-get install -y libbluetooth-dev bluetooth bluez-utils bluez usbutils
+apt-get install -y usbutils bluetooth bluez bluez-utils libbluetooth-dev 
