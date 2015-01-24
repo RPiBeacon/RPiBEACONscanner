8f112368b4db1e61e0b1c2b393ebf64280829726
Mattia <mattia.asti@gmail.com>
2015-01-24 17:46:53 +0000

    added start message

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var eventEmitter = new events.EventEmitter();
 var unirest = require('unirest');
 var location = process.env.RESIN_UUID;
 
+console.log('im started');
+
 // Start listening for iBeacon broadcast (ONLY iPHONE UUID)
 // bleacon.startScanning(['8492e75f4fd6469db132043fe94921d8', 'd0d3fa86ca7645ec9bd96af47e6df205', 'c262deb68d694b548263c9c244b21e8a'], true);
 bleacon.startScanning();
